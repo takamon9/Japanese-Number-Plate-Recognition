c8c561b1a1eadc3cfc77c8b8a3f8d80bc7187f5f
Yukihiro Takamoto <organowhatthehell1974@gmail.com>
2016-12-12 23:42:36 +0900

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@ int main(int, char**)
 
 	char fname[100];
 
-	int count = 0;
+	int countTimer = 0;
 
 	if (!capture.open(ip_address))
 	{
@@ -46,21 +46,21 @@ int main(int, char**)
 			resize(NumberPlate, resizedNP, Size(200, 100), 1.0, 1.0, INTER_LINEAR); //resize(src,dst,size(x,y),x_scale_factor,y_scale_factor,interpolation_type)
 			cvtColor(resizedNP, grayNP, CV_BGR2GRAY);
 
-		//	strftime(fname, sizeof(fname), "C:/ImageStorage/commercialNP/commNP_%y%m_%d_%H_%M_%S.png", localtime(&t));
-		//	imwrite(fname, grayNP);
-		//	count++;
-		//	cout << "We got Commercial Number Plate.No." << count << endl;
+			//	strftime(fname, sizeof(fname), "C:/ImageStorage/commercialNP/commNP_%y%m_%d_%H_%M_%S.png", localtime(&t));
+			//	imwrite(fname, grayNP);
+			//	countTimer++;
+			//	cout << "We got Commercial Number Plate.No." << countTimer << endl;
 
-			Mat binaryNP;
+			Mat binaryNP,cannyNP;
 			//blur(grayNP, grayNP, Size(3, 3), Point(-1, -1));
 			threshold(grayNP, binaryNP, 0, 255, THRESH_BINARY | THRESH_OTSU);
-			Canny(binaryNP, binaryNP, 3, 3);
-		//	imshow("binary", binaryNP);
+			Canny(binaryNP, cannyNP, 3, 3);
+			//	imshow("binary", binaryNP);
 			vector<vector<Point> > contours;
-			findContours(binaryNP, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+			findContours(cannyNP, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 			Mat contoursMat = resizedNP.clone();
-			Mat numRect[10];
-			int count = 0;
+			sort_struct sa[10];
+			vector<sort_struct> sortArray;
 
 			for (int i = 0; i < contours.size(); i++)
 			{
@@ -68,18 +68,25 @@ int main(int, char**)
 				double contourRatio = (float)pointArea.width / pointArea.height;
 				if (contourRatio > 0.4  && contourRatio < 0.6 && pointArea.height > 30 && pointArea.height < 55) {
 					rectangle(contoursMat, pointArea, Scalar(0, 255, 0), 1, 8);
-					//Rect numRoi(pointArea.x-2, pointArea.y-2, pointArea.width+2, pointArea.height+2);		
-					
-					numRect[count] = Mat(resizedNP,pointArea);	
-					stringstream nameImgFile;
-					nameImgFile << "img/num/left" << count << ".png";
-					imwrite(nameImgFile.str(), numRect[count]);
-					count++;
-
+					sortArray[i] = { pointArea.x, Mat(binaryNP,pointArea)};
 				}
 			}
 
-			imshow("num0", numRect[0]);
+			sort(sortArray.begin(),sortArray.end(),
+				[](const sort_struct& a, const sort_struct& b) {return (a.arrX < b.arrX); }
+			);
+
+			int count = 0;
+			for (int i = 0; i < 10; i++) {
+				if (sa[i].numRect.empty) {
+					break;
+				}
+				stringstream nameImgFile;
+				nameImgFile << "img/num/left" << count << ".png";
+				imwrite(nameImgFile.str(), sa[i].numRect);
+				count++;
+			}
+
 			imshow("contour", contoursMat);
 			imshow("captured number", resizedNP);
 		}
@@ -93,4 +100,3 @@ int main(int, char**)
 
 	return 0;
 }
-
