876c773f2eb0aa96c3bdd62fdbcdc72c84058d72
Yukihiro Takamoto <organowhatthehell1974@gmail.com>
2016-12-13 18:14:40 +0900

    Update Header.h

--- Header.h
+++ Header.h
@@ -2,16 +2,17 @@
 #define __HEADER_H_INCLUDED__
 
 #include <iostream>
-#include <opencv2\core\core.hpp>
-#include <opencv2\highgui\highgui.hpp>
 #include <opencv2\opencv.hpp>
-#include <opencv2\imgproc\imgproc.hpp>
 #include <fstream>
 #include <vector>
 #include <time.h>
+#include <math.h>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 using namespace cv;
+using namespace ml;
 
 Scalar colorNum(0, 255, 0);
 Scalar colorComm(255, 255, 0);
@@ -22,7 +23,7 @@ Mat original, image, gray;
 
 string uname = "root";
 string password = "taka1974";
-string ip_address = "http://" + uname + ":" + password + "@169.254.174.88/mjpg/video.mjpg";
+string ip_address = "http://" + uname + ":" + password + "@169.254.158.20/mjpg/video.mjpg";
 
 vector<Rect> commPlate;
 
@@ -30,11 +31,278 @@ VideoCapture capture;
 
 CascadeClassifier commNumPlate_cascade;
 
-struct sort_struct {
-	int arrX;
-	Mat numRect;
 
+struct sort_struct {
+	int xLocation;
+	Mat numRect;
 };
+
+const int N_INPUT = 200;
+const int N_HIDDEN = 10;
+const int N_OUTPUT = 10;
+
+vector<int> layer_sizes = { N_INPUT, N_HIDDEN, N_OUTPUT };
+
+Ptr<ANN_MLP> neuron = ANN_MLP::create();
+
+
+void *matrixArray(Mat matrixName, string fileN)
+{
+	fstream inFile;
+	stringstream fn;
+	fn << "dat/" << fileN << ".dat";
+	inFile.open(fn.str(), ios::in | ios::app | ios::binary);
+
+	for (int i = 0; i < matrixName.rows; i++) {
+		for (int j = 0; j < matrixName.cols; j++) {
+			if (matrixName.rows - i == 1 && matrixName.cols == j + 1) inFile << (int)(matrixName.at<uchar>(i, j)) / 255;
+			else inFile << (int)(matrixName.at<uchar>(i, j)) / 255;
+		}
+	}
+	inFile.close();
+	return 0;
+}
+
+char *matrixAbs()
+{
+	ifstream matrixAb;
+	//char fname[] =  "img/data.dat";
+	matrixAb.open("img/data.dat", ifstream::binary);
+	matrixAb.seekg(0, matrixAb.end);
+	int buffSize = matrixAb.tellg();
+	matrixAb.seekg(0, matrixAb.beg);
+
+	char *lines = new char[buffSize];
+	matrixAb.read(lines, buffSize);
+	cout << lines << endl;
+	return lines;
+	delete[] lines;
+}
+
+void trainNeuron() {
+
+	neuron->setTrainMethod(ANN_MLP::RPROP);
+	neuron->setLayerSizes(layer_sizes);
+	neuron->setActivationFunction(ANN_MLP::SIGMOID_SYM, 0.66, 1);
+	neuron->setTermCriteria(TermCriteria());
+
+	Mat number_data = (Mat_<float>(10, 200) <<
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1,
+		0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1,
+		0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1,
+		1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0,
+		1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+		0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1,
+		1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+		1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+		1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1,
+		1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+		0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1,
+		0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1,
+		1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+		0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
+		1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1,
+		1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+		0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1,
+		1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1,
+		1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
+		1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
+		0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0,
+		0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+		0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
+		1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1,
+		1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+		1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
+		1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+		0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1,
+		1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+		0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+		1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1,
+		1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1,
+		1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1,
+		1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1,
+		1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+		1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+		1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1,
+		1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1,
+		0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
+		1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0,
+		0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0,
+		0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1,
+		0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0,
+		0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+		0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1,
+		1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1
+		);
+	//		imshow("data", number_data);
+
+	Mat teacher = Mat(Size(10, 10), CV_32F);
+	for (int j = 0; j < teacher.rows; j++)
+		for (int i = 0; i < teacher.cols; i++)
+		{
+			if (i == j) teacher.at<float>(j, i) = 1.0f;
+			else teacher.at<float>(j, i) = 0.0f;
+		}
+
+	neuron->train(number_data, ROW_SAMPLE, teacher);
+
+	Mat test_data;
+	Mat results;
+	//	cout << fixed << right;
+	//	cout << endl;
+	//	cout << "      RPROP Neural Network Output				" << endl;
+	//	cout << "Input    0     1      2     3      4      5      6      7      8      9" << endl;
+
+	for (int i = 0; i < N_OUTPUT; i++)
+	{
+		test_data = number_data.row(i);
+		neuron->predict(test_data, results);
+		//	cout << setw(3) << i << " ";
+		/*	for (int j = 0; j < N_OUTPUT; j++)
+		{
+		cout << setw(6) << setprecision(3) << results.at<float>(0, j) << " ";
+		}
+		cout << endl;*/
+	}
+
+	double Max_value;
+	Point max_location;
+	//	cout << fixed << right;
+	//	cout << endl;
+	//	cout << "Input   Prediction　　Value" << endl;
+	//	cout << "------ -----------  ----------" << endl;
+	for (int i = 0; i < N_OUTPUT; i++)
+	{
+		test_data = number_data.row(i);
+		neuron->predict(test_data, results);
+		minMaxLoc(results, 0, &Max_value, 0, &max_location);
+		//		cout << setw(4) << i << "       " << max_location.x << "        " << Max_value << endl;
+	}
+}
+
+int processNeuralNetwork(string nameMatWindow) {
+
+	Ptr<ANN_MLP> neuron2 = ANN_MLP::create();
+
+	neuron2->setTrainMethod(ANN_MLP::RPROP);
+	neuron2->setLayerSizes(layer_sizes);
+	neuron2->setActivationFunction(ANN_MLP::SIGMOID_SYM, 0.66, 1);
+	neuron2->setTermCriteria(TermCriteria());
+
+	Mat original2, binaryOriginal2, result2, binaryPredict;
+
+
+	Mat teacher2 = Mat(Size(10, 1), CV_32F);
+	for (int j = 0; j < teacher2.rows; j++)
+		for (int i = 0; i < teacher2.cols; i++)
+		{
+			if (i == j) teacher2.at<float>(j, i) = 1.0f;
+			else teacher2.at<float>(j, i) = 0.0f;
+		}
+
+	stringstream detectNum;
+	string str = ".str()";
+	string fileN = nameMatWindow;
+	string folderN = "dat/";
+	string bb = fileN;
+	string pngN = ".png";
+	string cc = ".dat";
+	string strBuf = folderN + bb + cc;
+	string pngBuf = folderN + bb + pngN;
+/*
+//	detectNum << "img/num/" << fileN << ".png";
+//	original2 = imread(pngBuf);
+	if (original2.empty()) {
+		cout << "We cannot open the image file";
+		system("pause");
+	}
+	*/
+	//	cvtColor(original2, original2, CV_RGB2GRAY);
+	//	threshold(original2, original2, 0, 255, THRESH_BINARY | THRESH_OTSU);
+	//	resize(original2, binaryOriginal2, Size(10, 20));
+	//	imwrite("img/binaryOriginal2.png", binaryOriginal2);
+
+	FILE* fop;
+
+	fopen_s(&fop, strBuf.c_str(), "rb");
+	if (fop == NULL) {
+		cout << "We cannot open the dat file." << endl;
+		system("pause");
+		exit(0);
+	}
+
+	char buf[200];
+	int intbuf[200];
+	int f;
+	f = fread(buf, sizeof(unsigned char), 200, fop);
+	Mat binaryNumMat = Mat(Size(200, 1), CV_32F);
+	for (int i = 0; i < f; i++) {
+		intbuf[i] = buf[i] - 48;
+	//	cout << intbuf[i];
+	}
+
+	cout << endl;
+
+	for (int j = 0; j < binaryNumMat.rows; j++)
+		for (int i = 0; i < binaryNumMat.cols; i++)
+		{
+			if (intbuf[i] == 1) binaryNumMat.at<float>(j, i) = 1.0f;
+			else binaryNumMat.at<float>(j, i) = 0.0f;
+		}
+
+	//	imshow("binaryNumMat", binaryNumMat);
+
+	fclose(fop);
+	cout << endl;
+
+	neuron2->train(binaryNumMat, ROW_SAMPLE, teacher2);
+
+	//	imshow("predictNum", binaryPredictArray);
+	Mat neuralMat = binaryNumMat.row(0);
+	Point maxLocationResult;
+	neuron2->predict(neuralMat, result2);
+	minMaxLoc(result2, NULL, NULL, NULL, &maxLocationResult);
+	//	cout << "Detected Number is; " << maxLocationResult.x << endl;
+	return maxLocationResult.x;
+}
 
 #endif
 
